426339f5a0b7344edae7e69c23c60e72aa001a6e
Daniel Saulin <saulin.daniel33@gmail.com>
2023-03-03 22:13:17 +0300

    Создал стратовый контроллер

---
+++ CleanOff/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleanOff.Controllers;
+
+public class HomeController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return Content("Hello world");
+    }
+}
