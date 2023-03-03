5c4625afb533df47003c3f2662720f473a5ac946
Daniel Saulin <saulin.daniel33@gmail.com>
2023-03-03 22:12:59 +0300

    Добавил MVC в сервисы, настроил стандартный роутинг

--- CleanOff/Program.cs
+++ CleanOff/Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
+
+
+// Service settings
+builder.Services.AddControllersWithViews();
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{Id?}"
+);
 
 app.Run();
