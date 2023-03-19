36bd6f8b84253ef06313f1e5b16ded7d4b321302
Daniel Saulin <saulin.daniel33@gmail.com>
2023-03-20 01:29:15 +0300

    Добавил ApiController.cs

---
+++ CleanOff/Controllers/ApiController.cs
@@ -0,0 +1,43 @@
+﻿using CleanOff.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanOff.Controllers;
+
+public class ApiController : Controller
+{
+    public IActionResult Login()
+    {
+        return BadRequest();
+    }
+
+    public IActionResult Register()
+    {
+        return BadRequest();
+    }
+
+    public IActionResult GetUserOrders(Client client)
+    {
+        return BadRequest();
+    }
+
+    public IActionResult GetOrderItems()
+    {
+        return BadRequest();
+    }
+
+    public IActionResult GetOrderInfo(Order order)
+    {
+        return BadRequest();
+    }
+
+    public IActionResult CreateOrder(Order order)
+    {
+        return BadRequest();
+    }
+
+    public IActionResult UpdateOrder(Order order)
+    {
+        return BadRequest();
+    }
+    
+}
