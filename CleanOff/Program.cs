916ce9bdda2c368fc2008e80a9343f3d3d0ace7c
Daniel Saulin <saulin.daniel33@gmail.com>
2023-03-20 02:06:16 +0300

    Создал первую миграю бд

--- CleanOff/Program.cs
+++ CleanOff/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using CleanOff.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 
 // Service settings
 builder.Services.AddControllersWithViews();
+builder.Configuration.AddJsonFile("connection_strings.json");
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 
 var app = builder.Build();
