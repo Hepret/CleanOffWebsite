6e42d2df5dc012e2b7ac6ae68735518d430eadb7
Nikita00721 <nkremnev3003@gmail.com>
2023-03-19 20:47:50 +0300

    Cделана страница логина по нажатию на кнопку "личный кабинет"

---
+++ CleanOff/wwwroot/js/loginscript.js
@@ -0,0 +1,9 @@
+﻿function switchForms() {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("registration").style.display = "block";
+}
+
+function switchFormsback() {
+    document.getElementById("registration").style.display = "none";
+    document.getElementById("login").style.display = "block";
+}
