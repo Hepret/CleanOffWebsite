64db31f44094a1c9bacfce1464330f7109222cec
Daniel Saulin <saulin.daniel33@gmail.com>
2023-03-22 02:05:16 +0300

    Добавил методы для получения: - клиентов - заказов - доступных услуг - сотрудников

--- CleanOff/Controllers/ApiController.cs
+++ CleanOff/Controllers/ApiController.cs
@@ -12,5 +12,32 @@ public class ApiController : ControllerBase
     {
         _dbContext = dbContext;
     }
+
+    [HttpGet]
+    public IEnumerable<Client> GetClients()
+    {
+        return _dbContext.Clients;
+    }
     
+    [HttpGet]
+    public IEnumerable<Order> GetOrders()
+    {
+        return _dbContext.Orders;
+    }
+
+    [HttpGet]
+    public IEnumerable<OrderItem> GetItems()
+    {
+        return _dbContext.OrderItems;
+    }
+
+    [HttpGet]
+    public IEnumerable<Employee> GetEmployees()
+    {
+        return _dbContext.Employees;
+    }
+
+    
+
+
 }
