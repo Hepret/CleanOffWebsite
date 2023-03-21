ff92936e97fce754c79199d1c95fa8ccec9813db
Daniel Saulin <saulin.daniel33@gmail.com>
2023-03-22 01:33:15 +0300

    Добавил атрибуты к для генерации Primary Keys для таблицы OrderItem

--- CleanOff.Domain/OrderItem.cs
+++ CleanOff.Domain/OrderItem.cs
@@ -1,8 +1,13 @@
-﻿namespace CleanOff.Domain;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CleanOff.Domain;
 
 public class OrderItem
 {
-    public int Id { get; set; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Key]
+    public Guid OrderItemId { get; set; }
     public string Name { get; set; }
     public decimal Price { get; set; }
     public string Description { get; set; }
