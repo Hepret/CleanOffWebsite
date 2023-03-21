6cced397742f518c06d36b31cbd3244a323c828e
Daniel Saulin <saulin.daniel33@gmail.com>
2023-03-22 01:40:21 +0300

    Переименовал DataContext в ApplicationDbContext. Добавил таблицу OrderItems, OrderItemsWithAmount

---
+++ CleanOff/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CleanOff.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanOff.Models;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<OrderItems> OrderItemsWithAmount { get; set; }
+}
