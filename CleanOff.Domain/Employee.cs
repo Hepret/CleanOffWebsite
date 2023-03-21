51ee8d81b93640f7d5c98c9051e985f7d692700c
Daniel Saulin <saulin.daniel33@gmail.com>
2023-03-22 01:31:47 +0300

    Добавил атрибуты к для генерации Primary Keys для таблицы Employee

--- CleanOff.Domain/Employee.cs
+++ CleanOff.Domain/Employee.cs
@@ -1,8 +1,13 @@
-﻿namespace CleanOff.Domain;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CleanOff.Domain;
 
 public class Employee
 {
-    public string Id { get; set; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Key]
+    public Guid EmployeeId { get; set; }
     public string Name { get; set; }
     public string Phone { get; set; }
 }
