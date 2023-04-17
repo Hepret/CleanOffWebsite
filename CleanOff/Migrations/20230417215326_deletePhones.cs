fff4e4d2ffe359b78f32f42c45df27a900033cce
Daniel Saulin <saulin.daniel33@gmail.com>
2023-04-18 00:56:48 +0300

    Новые миграции

---
+++ CleanOff/Migrations/20230417215326_deletePhones.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanOff.Migrations
+{
+    /// <inheritdoc />
+    public partial class deletePhones : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Phone",
+                table: "Clients",
+                newName: "Surname");
+
+            migrationBuilder.RenameColumn(
+                name: "Mail",
+                table: "Clients",
+                newName: "PasswordHash");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Clients",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Clients");
+
+            migrationBuilder.RenameColumn(
+                name: "Surname",
+                table: "Clients",
+                newName: "Phone");
+
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Clients",
+                newName: "Mail");
+        }
+    }
+}
