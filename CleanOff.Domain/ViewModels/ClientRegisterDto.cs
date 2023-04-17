1bcbf5df794f209ca429e82dc99c475b46e06c37
Daniel Saulin <saulin.daniel33@gmail.com>
2023-04-17 17:35:47 +0300

    Отрефакторил ClientRegisterDto.cs

--- CleanOff.Domain/ViewModels/ClientRegisterDto.cs
+++ CleanOff.Domain/ViewModels/ClientRegisterDto.cs
@@ -2,8 +2,7 @@
 
 public class ClientRegisterDto
 {
-    public string Name { get; set; }
-    public string Phone { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
+    public string Name { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
 }
