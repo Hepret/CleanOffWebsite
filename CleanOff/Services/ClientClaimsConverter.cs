1cd9bc4ba2718f96d10d78c8fc8bff78bec8dcbf
Daniel Saulin <saulin.daniel33@gmail.com>
2023-04-17 15:53:33 +0300

    Добавил класс для создания Claims для аутентификации

---
+++ CleanOff/Services/ClientClaimsConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+using CleanOff.Domain;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace CleanOff.Services;
+
+public static class ClientClaimsConverter
+{
+    public static ClaimsPrincipal Convert(Client client)
+    {
+        var claims = new Claim[]
+        {
+            new Claim("id", client.ClientId.ToString()),
+            new Claim(ClaimTypes.Role, "Client")
+        };
+
+        var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        var claimsPrincipal = new ClaimsPrincipal(identity);
+        return claimsPrincipal;
+    }
+}
