586fa8285b8311a4b9a091dbf5e40a6e3b699458
Daniel Saulin <saulin.daniel33@gmail.com>
2023-04-17 16:04:32 +0300

    Добавил класс ошибку ClientManager.cs

---
+++ CleanOff/Exceptions/ClientAlreadyExistException.cs
@@ -0,0 +1,15 @@
+﻿using CleanOff.Domain;
+
+namespace CleanOff.Services;
+
+public class ClientAlreadyExistException : ArgumentException
+{
+    private readonly Client _client;
+    
+    public ClientAlreadyExistException(Client client) : base($"Client with email {client.Email} already Exist")
+    {
+        _client = client;
+    }
+    
+    
+}
