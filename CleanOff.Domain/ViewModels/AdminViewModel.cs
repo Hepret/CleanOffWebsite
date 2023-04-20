8da2b5f37305d23b9bdf5596544e7c7a1c0488fc
Daniel Saulin <saulin.daniel33@gmail.com>
2023-04-20 22:58:29 +0300

    Добавил AdminViewModel.cs

---
+++ CleanOff.Domain/ViewModels/AdminViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CleanOff.Domain.Users;
+
+namespace CleanOff.ViewModels;
+
+public class AdminViewModel
+{
+    public Guid AdminId { get; private set; }
+    public string Name { get; private set; } 
+    public string Surname { get; private set; } 
+    public string Email { get; private set; } 
+    
+    public AdminViewModel(Admin admin)
+    {
+        AdminId = admin.AdminId;
+        Name = admin.Name;
+        Surname = admin.Surname;
+        Email = admin.Email;
+    }
+}
