38ffdc6c76bc42ed1dce850f041191ee1dd25367
Daniel Saulin <saulin.daniel33@gmail.com>
2023-04-20 21:02:24 +0300

    Новая миграция

---
+++ CleanOff/Migrations/20230420180111_Добавил таблицу admins.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanOff.Migrations
+{
+    /// <inheritdoc />
+    public partial class Добавилтаблицуadmins : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Employees",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "HashPassword",
+                table: "Employees",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Surname",
+                table: "Employees",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    AdminId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Surname = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.AdminId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admins");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "HashPassword",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Surname",
+                table: "Employees");
+        }
+    }
+}
