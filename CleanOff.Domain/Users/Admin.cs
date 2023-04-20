0a713b1acff569382db1fa78126ad13f8f53e962
Daniel Saulin <saulin.daniel33@gmail.com>
2023-04-20 19:19:41 +0300

    Создал модель администратора

---
+++ CleanOff.Domain/Users/Admin.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanOff.Domain.Users;
+
+public class Admin 
+{
+    public Guid AdminId { get; set; }
+    public string Name { get; set; } = null!;
+    public string Surname { get; set; } = null!;
+    public string HashPassword { get; set; } = null!;
+    public string Email { get; set; } = null!;
+}
