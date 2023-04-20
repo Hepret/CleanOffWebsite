2b05e517eb4938a643c9571f0d27e6aa1aa8c1d8
Daniel Saulin <saulin.daniel33@gmail.com>
2023-04-20 20:13:34 +0300

    Изменил модель и вью-модель Администратора

--- CleanOff.Domain/Users/Admin.cs
+++ CleanOff.Domain/Users/Admin.cs
@@ -1,4 +1,6 @@
-﻿namespace CleanOff.Domain.Users;
+﻿using CleanOff.ViewModels;
+
+namespace CleanOff.Domain.Users;
 
 public class Admin 
 {
@@ -7,4 +9,12 @@ public class Admin
     public string Surname { get; set; } = null!;
     public string HashPassword { get; set; } = null!;
     public string Email { get; set; } = null!;
+    
+    public Admin(AdminRegisterDto registerDto){
+        AdminId = Guid.NewGuid();
+        Name = registerDto.Name;
+        HashPassword = BCrypt.Net.BCrypt.HashPassword(registerDto.Password);
+        Surname = registerDto.Surname;
+        Email = registerDto.Email;
+    }
 }
---
+++ CleanOff.Domain/ViewModels/AdminRegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanOff.ViewModels;
+
+public class AdminRegisterDto
+{
+    public string Name { get; set; } = null!;
+    public string Surname { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string Email { get; set; } = null!;
+}
