01d366575cba7a43f2fc7ab0dc98682a7281a63d
Daniel Saulin <saulin.daniel33@gmail.com>
2023-04-20 19:02:53 +0300

    Создал сервис для добавления и поиска работников

---
+++ CleanOff/Services/EmployeeManager.cs
@@ -0,0 +1,42 @@
+﻿using CleanOff.Domain;
+using CleanOff.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanOff.Services;
+
+public class EmployeeManager : IEmployeeManager
+{
+    private readonly ApplicationDbContext _context;
+
+    public EmployeeManager(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CreateAsync(Employee employee)
+    {
+        await _context.Employees.AddAsync(employee);
+    }
+
+    public async Task<List<Employee>> GetAllEmployees()
+    {
+        return await _context.Employees.ToListAsync();
+    }
+
+    public bool VerifyPassword(Employee employee, string password)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, employee.HashPassword);
+    }
+
+    public async Task<Employee?> FindById(Guid id)
+    {
+        return await _context.Employees.FirstOrDefaultAsync(e => e.EmployeeId == id)!;
+    }
+
+    public async Task<Employee?> FindByEmail(string email)
+    {
+        return await _context.Employees.FirstOrDefaultAsync(e => e.Email == email)!;
+
+    }
+    
+}
---
+++ CleanOff/Services/IEmployeeManager.cs
@@ -0,0 +1,11 @@
+﻿using CleanOff.Domain;
+
+namespace CleanOff.Services;
+
+public interface IEmployeeManager
+{
+    public Task CreateAsync(Employee employee);
+    public bool VerifyPassword(Employee employee, string password);
+    public Task<Employee?> FindById(Guid id);
+    public Task<Employee?> FindByEmail(string email);
+}
