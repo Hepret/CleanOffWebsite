a6cc7f725d9e5f62c2d43ac82b36573b70cb44a6
Daniel Saulin <saulin.daniel33@gmail.com>
2023-04-20 19:32:01 +0300

    Добавил класс конвертор администратора в claimsPrincipals

---
+++ CleanOff/Services/AdminClaimsConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Claims;
+using CleanOff.Domain;
+using CleanOff.Domain.Users;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace CleanOff.Services;
+
+public class AdminClaimsConverter
+{
+    public static ClaimsPrincipal Convert(Admin admin)
+    {
+        var claims = new Claim[]
+        {
+            new Claim("id", admin.AdminId.ToString()),
+            new Claim(ClaimTypes.Role, "Admin")
+        };
+
+        var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        var claimsPrincipal = new ClaimsPrincipal(identity);
+        return claimsPrincipal;
+    }
+}
