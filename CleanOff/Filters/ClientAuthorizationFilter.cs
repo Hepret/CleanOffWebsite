42f64ba5e442500d0d3c0af1accba7cfa34a4780
Daniel Saulin <saulin.daniel33@gmail.com>
2023-04-20 18:00:42 +0300

    Добавил фильтр авторизации который будет переносить неавторизовоного клиента на страницу регистрации

---
+++ CleanOff/Filters/ClientAuthorizationFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CleanOff.Filters;
+
+public class ClientAuthorizationFilter : Attribute, IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        var user = context.HttpContext.User;
+        if (user.Identity != null && (!user.Identity.IsAuthenticated || !user.IsInRole("Client")))
+        {
+            context.Result = new RedirectToActionResult("Index","Profile", null);
+            
+        }
+        return;
+    }
+}
