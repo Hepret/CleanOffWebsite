a9155a2b3d75c2e8e3ca50a0af69f3198f2a3611
Daniel Saulin <saulin.daniel33@gmail.com>
2023-04-20 23:01:48 +0300

    Создал AdminAuthorizeFilter.cs

---
+++ CleanOff/Filters/AdminAuthorizeFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CleanOff.Filters;
+
+public class AdminAuthorizeFilter : Attribute, IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        var user = context.HttpContext.User;
+        if (user.Identity != null && (!user.Identity.IsAuthenticated || !user.IsInRole("Admin")))
+        {
+            context.Result = new RedirectToActionResult("Login","Admin", null);
+            
+        }
+        return;
+    }
+}
