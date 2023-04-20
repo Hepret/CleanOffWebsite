b695c5d0939649f51c403e05b00d9650266fc0e7
Daniel Saulin <saulin.daniel33@gmail.com>
2023-04-20 18:15:24 +0300

    Добавил контроллер для панели администратора

---
+++ CleanOff/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleanOff.Controllers;
+
+[Route("admin")]
+public class AdminController : Controller
+{
+    [HttpGet("admin")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    
+    
+    
+}
