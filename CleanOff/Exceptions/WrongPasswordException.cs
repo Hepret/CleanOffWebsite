ba1343ddc40e5a83f3219229a1275f790f7ad0bb
Daniel Saulin <saulin.daniel33@gmail.com>
2023-04-20 20:45:40 +0300

    Добавил ошибку WrongPasswordException

---
+++ CleanOff/Exceptions/WrongPasswordException.cs
@@ -0,0 +1,8 @@
+﻿namespace CleanOff.Exceptions;
+
+public class WrongPasswordException<T> : ArgumentException
+{
+    public WrongPasswordException(string email) : base($"{typeof(T).Name} с почтой: {email}")
+    {
+    }
+}
