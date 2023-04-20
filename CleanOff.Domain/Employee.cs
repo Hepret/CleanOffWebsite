ab515e8e2ab498ef5dc5eb5743169ad12d287c08
Daniel Saulin <saulin.daniel33@gmail.com>
2023-04-20 19:04:32 +0300

    Исправил класс работника

--- CleanOff.Domain/Employee.cs
+++ CleanOff.Domain/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using CleanOff.ViewModels;
 
 namespace CleanOff.Domain;
 
@@ -8,6 +9,22 @@ public class Employee
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     [Key]
     public Guid EmployeeId { get; set; }
+    public string HashPassword { get; set; }
     public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
     public string Phone { get; set; }
+
+    public Employee()
+    { }
+
+    public Employee(EmployeeRegisterDto registerDto)
+    {
+        EmployeeId = Guid.NewGuid();
+        HashPassword = BCrypt.Net.BCrypt.HashPassword(registerDto.Password);
+        Name = registerDto.Name;
+        Surname = registerDto.Surname;
+        Email = registerDto.Email;
+        Phone = registerDto.Phone;
+    }
 }
