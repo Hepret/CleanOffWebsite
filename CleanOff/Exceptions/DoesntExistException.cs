7035e1bd5cf94ec025fd11a59780639ebbc0d5c8
Daniel Saulin <saulin.daniel33@gmail.com>
2023-04-20 20:29:08 +0300

    Добавил ошибку DoesntExistException

---
+++ CleanOff/Exceptions/DoesntExistException.cs
@@ -0,0 +1,8 @@
+﻿namespace CleanOff.Exceptions;
+
+public class DoesntExistException : ArgumentException
+{
+    public DoesntExistException(string email) : base($"Человека с электронной почтой: {email} - не существует в базе данных")
+    {
+    }
+}
