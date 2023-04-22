3ae9a8103d3c822075371375caba249b6db78062
Daniel Saulin <saulin.daniel33@gmail.com>
2023-04-22 16:11:01 +0300

    Создал AdminController и представления для него

--- CleanOff/Controllers/AdminController.cs
+++ CleanOff/Controllers/AdminController.cs
@@ -1,17 +1,100 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CleanOff.Domain;
+using CleanOff.Domain.Users;
+using CleanOff.Domain.ViewModels;
+using CleanOff.Exceptions;
+using CleanOff.Filters;
+using CleanOff.Models;
+using CleanOff.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CleanOff.Controllers;
 
-[Route("admin")]
+[Route("/{controller}")]
+[Controller]
 public class AdminController : Controller
 {
-    [HttpGet("admin")]
-    public IActionResult Index()
+    private readonly ApplicationDbContext _dbContext;
+    private readonly IEmployeeManager _employeeManager;
+    private readonly IAdminManager _adminManager;
+    
+    
+    public AdminController(ApplicationDbContext dbContext, IEmployeeManager employeeManager, IAdminManager adminManager)
+    {
+        _dbContext = dbContext;
+        _employeeManager = employeeManager;
+        _adminManager = adminManager;
+    }
+    [AdminAuthorizeFilter]
+    [HttpGet("/admin")]
+    public async Task<IActionResult> Index()
+    {
+        var employees = await _employeeManager.GetAllEmployees();
+        List<EmployeeViewModel> employeeViewModels = employees
+            .Select(employee => new EmployeeViewModel(employee))
+            .ToList();
+        return View(employeeViewModels);
+    }
+    [AdminAuthorizeFilter]
+    [HttpGet("/admin/createEmployee")]
+    public IActionResult CreateEmployee()
     {
         return View();
     }
-    
-    
-    
-    
-}
+    [AdminAuthorizeFilter]
+    [HttpPost("/admin/createEmployee")]
+    public async Task<IActionResult> CreateEmployee(EmployeeRegisterDto registerDto)
+    {
+        var employee = new Employee(registerDto);
+
+        try
+        {
+            await _employeeManager.CreateAsync(employee);
+            TempData["EmployeeCreation"] = "succeed"; 
+            return RedirectToAction("Index");
+        }
+        catch (Exception e)
+        {
+            TempData["EmployeeCreation"] = "failed"; 
+            return RedirectToAction("Index");
+        }
+    }
+
+    [HttpGet("/admin/login")]
+    [AllowAnonymous]
+    public IActionResult Login()
+    {
+        return View();
+    }
+    [AllowAnonymous]
+    [HttpPost("/admin/login")]
+    public async Task<IActionResult> Login(AdminLoginDto loginDto)
+    {
+        var email = loginDto.Email;
+        var admin = await _adminManager.FindByEmailAsync(email);
+        if (admin == null) throw new DoesntExistException(email);
+        var verifyPassword = _adminManager.VerifyPassword(admin, loginDto.Password);
+        if (!verifyPassword) throw new WrongPasswordException<Admin>(email);
+        await Authenticate(admin);
+        return RedirectToAction("Index");
+    }
+
+    [AdminAuthorizeFilter]
+    [HttpGet("profile")]
+    public async Task<IActionResult> Profile()
+    {
+        var usr = User;
+        var stringId = User.Claims.First(claim => claim.Type == "id").Value;
+        var id = Guid.Parse(stringId);
+        var admin = await _adminManager.FindByIdAsync(id);
+        return View(admin!);
+    }
+
+    [NonAction]
+    async Task Authenticate(Admin admin)
+    {
+        var claimsPrincipals = AdminClaimsConverter.Convert(admin);
+        await HttpContext.SignInAsync(claimsPrincipals);
+    }
+}   
---
+++ CleanOff/Views/Admin/_AdminPanelLayout.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CleanOff.Views.Admin;
+
+public class Layout : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
