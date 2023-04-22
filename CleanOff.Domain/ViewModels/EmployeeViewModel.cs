c9a52d2a2bf2b7cbdf95bf84c10751ce4d6a980a
Daniel Saulin <saulin.daniel33@gmail.com>
2023-04-22 16:10:22 +0300

    Рефакторинг EmployeeViewModel.cs

---
+++ CleanOff.Domain/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace CleanOff.Domain.ViewModels;
+
+public class EmployeeViewModel
+{
+    public string Name { get; private set; }
+    public string Surname { get; private set; } 
+    public string Email { get; private set; }
+    public Guid EmployeeId { get; private set; }
+
+    public EmployeeViewModel(Employee employee)
+    {
+        Name = employee.Name;
+        Surname = employee.Surname;
+        Email = employee.Email;
+        EmployeeId = employee.EmployeeId;
+    }
+    
+}
