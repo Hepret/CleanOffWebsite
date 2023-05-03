45d8f5b8718c702d10cbe589f51e1db5a2859d85
Daniel Saulin <saulin.daniel33@gmail.com>
2023-05-03 14:40:38 +0300

    Создал конвертер из модели работника в claims

---
+++ CleanOff/Services/EmployeeClaimsConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+using CleanOff.Domain;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace CleanOff.Services;
+
+public static class EmployeeClaimsConverter
+{
+    public static ClaimsPrincipal Convert(Employee employee)
+    {
+        var claims = new Claim[]
+        {
+            new Claim("id", employee.EmployeeId.ToString()),
+            new Claim(ClaimTypes.Role, "Employee")
+        };
+
+        var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        var claimsPrincipal = new ClaimsPrincipal(identity);
+        return claimsPrincipal;
+    }
+}
