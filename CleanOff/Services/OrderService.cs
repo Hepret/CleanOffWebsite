d6958795509630adec239cd98ceddb33ecbae002
Daniel Saulin <saulin.daniel33@gmail.com>
2023-05-03 23:58:56 +0300

    Добавил сервис для добавления, изменения, удаления заказов

---
+++ CleanOff/Services/OrderService.cs
@@ -0,0 +1,30 @@
+﻿using CleanOff.Domain;
+using CleanOff.Models;
+
+namespace CleanOff.Services;
+
+public class OrderService
+{
+    private readonly ApplicationDbContext _context;
+
+    public OrderService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CreateOrder(Order order)
+    {
+        try
+        {
+            await _context.Orders.AddAsync(order);
+            // TODO Логирование создание заказа
+        }
+        catch
+        {
+            // TODO Логирование ошибка создание заказа
+            throw;
+        }
+        
+    }
+    
+}
